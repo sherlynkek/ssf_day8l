70751583d49cad3a1231ebb916a76a7952744166
sherlynkek <sherlynkek00@gmail.com>
2024-12-05 12:20:23 +0800

    Session

--- src/main/java/sg/edu/nus/iss/vttp5a_day8l/controller/SessionController.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_day8l/controller/SessionController.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
